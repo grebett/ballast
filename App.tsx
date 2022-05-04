7d0ebd36cd76f466dfe88528129db1525b89f6a2
Grégory Alan <contact@grebett.com>
2022-05-04 14:38:21 +0200

    feat: react-native-gesture-handler

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  GestureDetector,
+  Gesture,
+  Directions,
+} from "react-native-gesture-handler";
 
 export default function App() {
+  const flingLeft = Gesture.Fling();
+  flingLeft.direction(Directions.LEFT).onEnd(console.log);
+
+  const flingRight = Gesture.Fling();
+  flingRight.direction(Directions.RIGHT).onEnd(console.log);
+
+  const flings = Gesture.Simultaneous(flingLeft, flingRight)
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureDetector gesture={flings}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app or not!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </GestureDetector>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
