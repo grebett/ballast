01e27546d748628d55d36cc683fcf1a6fa395314
Grégory Alan <contact@grebett.com>
2022-05-04 16:08:56 +0200

    feat: react-navigation & tempfix: react-native-gesture-handler
    2.4.1 is bugged and should be fixed soon (see https://github.com/software-mansion/react-native-gesture-handler/commit/19a6e0fa693983ff1697d7c58357d886fbac3f98)

--- App.tsx
+++ App.tsx
@@ -1,33 +1,52 @@
-// import { useState, useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect, PropsWithChildren } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import {
   GestureDetector,
   Gesture,
   Directions,
-} from "react-native-gesture-handler";
+} from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { playSound, fadeOutSound } from './services/sound';
 
-export default function App() {
-  const fling = Gesture.Fling();
-  fling.direction(Directions.LEFT | Directions.RIGHT).onEnd(() => playSound());
+const Component = () => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#faf',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
   return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app or not!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const HomeScreen = () => {
+  const fling = Gesture.Fling();
+  fling.direction(Directions.LEFT | Directions.RIGHT).onEnd(() => playSound());
+  return (
     <GestureDetector gesture={fling}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app or not!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Component />
     </GestureDetector>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
