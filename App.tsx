1d20d815c510856113deef128ad6daf485067462
Grégory Alan <contact@grebett.com>
2022-05-10 17:12:19 +0200

    feat: splash screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, PropsWithChildren } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import {
@@ -11,6 +11,7 @@ import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { playSound } from './services/sound';
 import { AnotherComponent } from './AnotherComponent'; // TEMP OTHER_COMPONENT
@@ -84,18 +85,50 @@ const SecondScreen = ({
 const Stack = createNativeStackNavigator<StackParamList>();
 export default function App() {
   const [rootStore, setRootStore] = useState<RootStore | undefined>(undefined);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-    setupRootStore().then(setRootStore);
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        setupRootStore().then(setRootStore);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
 
-  if (!rootStore) return null;
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady || !rootStore) {
+    return null;
+  }
+
   return (
     <RootStoreProvider value={rootStore}>
-      <NavigationContainer>
+      <NavigationContainer onReady={onLayoutRootView}>
         <Stack.Navigator>
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="SecondScreen" component={SecondScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="SecondScreen" component={SecondScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </RootStoreProvider>
